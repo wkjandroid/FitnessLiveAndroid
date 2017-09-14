53b9b1603a6c14a334ddc667a969d520a6d4c95b
wangkaijin <17862901468@163.com>
2017-09-14 14:15:05 +0800

    9-14

--- app/src/main/java/com/example/wkj_pc/fitnesslive/activity/VideoPlayerActivity.java
+++ app/src/main/java/com/example/wkj_pc/fitnesslive/activity/VideoPlayerActivity.java
@@ -2,8 +2,11 @@ package com.example.wkj_pc.fitnesslive.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 import com.example.wkj_pc.fitnesslive.R;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
@@ -12,7 +15,22 @@ public class VideoPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
         String videourl = getIntent().getStringExtra("videourl");
-
-
+        JCVideoPlayerStandard jcVideoPlayerStandard = (JCVideoPlayerStandard) findViewById(R.id.videoplayer);
+        jcVideoPlayerStandard.setUp("http://47.93.20.45:8080/a.mp4"
+                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "马上开始");
+        ImageView videoView = jcVideoPlayerStandard.thumbImageView;
+        Glide.with(this).load("http://47.93.20.45:8080/tomcat.png").asBitmap().into(videoView);
+    }
+    @Override
+    public void onBackPressed() {
+        if (JCVideoPlayer.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JCVideoPlayer.releaseAllVideos();
     }
 }
